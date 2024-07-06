684fd53590026327cb0b5a8ebec9abb9074fa1a7
Katie Ludwig <katierludwig@gmail.com>
2024-07-06 16:45:00 -0700

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1234567890123',
+        make: 'Ford',
+        model: 'Mustang',
+        mileage: 10000,
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '1234567890123',
+        make: 'Chevrolet',
+        model: 'Impala',
+        mileage: 20000,
+        title: 'salvage',
+    },
+    {
+        vin: '1234567890123',
+        make: 'Oldsmobile',
+        model: 'Cutlass',
+        mileage: 40000,
+    }
+]
+
+exports.seed = function(knex) {
+    return knex('cars')
+    .truncate().then(() => {
+        return knex('cars').insert(cars);
+    })
+};
