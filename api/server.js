4b1af4d6758c419efef79d32cf2c644d3aaa576f
Katie Ludwig <katierludwig@gmail.com>
2024-07-06 19:14:33 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -1,7 +1,18 @@
-const express = require("express")
+const express = require("express");
 
-const server = express()
+const server = express();
 
+server.use(express.json());
 
+server.use("*", (req, res, next) => {
+    next({ status: 404, message: "Not found" });
+})
+
+server.use((err, req, res, next) => { //eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack
+    });
+});
 
 module.exports = server
